4332a0ff3ca021a283a5b663e7f9e891befb2690
GhostBot <jesonnet@ulb.ac.be>
2015-03-12 21:23:41 +0100

    fuck

--- src/out.py
+++ src/out.py
@@ -1,17 +1,37 @@
 
-class Out:
-    
-	def __init__(self, affectation):
-		self.a = affectation
+from src import key
 
-	def writeFile():
-		f = open('output', 'w')
-		for i in self.a.row:
-			line = "{} {} {}/n".format(a[i].rang, a[i].place, a[i].group)
-			f.write(line)
 
-	def writeTest():
-		f = open('output', 'w')
-		for s in range(10):
-			line = "{} {} {}/n".format("s.rang", "s.place", "s.group")
-			f.write(line)
+def writeFile( M , affectations ) :
+	'''server, interval, position, group'''
+
+	if not M : return
+
+	f = open( 'out/out' , 'w' )
+	
+	affectations = sorted( affectations , key = key.id )
+
+	with open( 'out/out' , 'w' ) :
+
+		j = 0
+
+		for i in range( M ) :
+
+			if j < len( affectations ) :
+
+				affectation = affectations[j]
+
+				if affectation.id == i :
+
+					pos = affectation.position + affectation.interval.start
+					row = affectation.interval.row
+
+					line = "{} {} {}\n".format(i, pos, row)
+					j += 1
+
+				else : line = 'x\n'
+
+			else : line = 'x\n'
+
+			f.write( line )
+
