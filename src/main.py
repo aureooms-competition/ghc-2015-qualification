86d6a31031eb54d4473cf756031f3a5f82358bf9
aureooms <aurelien.ooms@gmail.com>
2015-03-12 19:14:06 +0100

    make main compile for others

--- src/main.py
+++ src/main.py
@@ -1,19 +1,4 @@
 
-from src.helloworld import helloworld
-
-import src.lp as lp
-
 def main ( ) :
 
-	print( main , helloworld )
-
-	helloworld( )
-
-	c = 0
-
-	for i in range( 10000000 ) : c += i
-
-	print( c )
-
-	lp.test1( )
-	lp.test2( )
+	pass
