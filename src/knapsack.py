f54d0cfb2bdf20e481d16813d4db9f24c6cdbf1d
aureooms <aurelien.ooms@gmail.com>
2015-03-14 17:04:21 +0100

    fix for no glpk

--- src/knapsack.py
+++ src/knapsack.py
@@ -1,87 +1,98 @@
 
-import ecyglpki as glpk
+try :
 
-def multidimensional ( D , N , v , w , W ) :
+	import ecyglpki as glpk
 
-	x = lambda d , i  : d * N + i + 1
+	def multidimensional ( D , N , v , w , W ) :
 
-	lp = glpk.Problem( )
+		x = lambda d , i  : d * N + i + 1
 
-	lp.add_cols( D * N )
+		lp = glpk.Problem( )
 
-	lp.add_rows( D + N )
+		lp.add_cols( D * N )
 
-	for d in range( D ) :
+		lp.add_rows( D + N )
 
-		for i in range( N ) :
+		for d in range( D ) :
 
-			lp.set_col_kind( x( d , i ) , "binary" )
-			lp.set_obj_coef( x( d , i ) , v[i] )
+			for i in range( N ) :
 
-		mapping = { x( d , i ) : w[i] for i in range( N )  }
+				lp.set_col_kind( x( d , i ) , "binary" )
+				lp.set_obj_coef( x( d , i ) , v[i] )
 
-		lp.set_mat_row( d + 1 , mapping )
+			mapping = { x( d , i ) : w[i] for i in range( N )  }
 
-		lp.set_row_bnds( d + 1 , None , W[d] )
+			lp.set_mat_row( d + 1 , mapping )
 
-	# a resource is used only once
+			lp.set_row_bnds( d + 1 , None , W[d] )
 
-	for i in range( N ) :
-
-		mapping = { x( d , i ) : 1 for d in range( D ) }
-
-		lp.set_mat_row( D + i + 1 , mapping )
-
-		lp.set_row_bnds( D + i + 1 , None , 1 )
-
-	lp.set_obj_dir( 'maximize' )
-
-	return lp
-
-
-def solve ( D , N , lp ) :
-
-	x = lambda d , i  : d * N + i + 1
-
-	# configure and solve
-
-	iocp = glpk.IntOptControls() # (default) int. opt. control parameters
-	iocp.presolve = True
-
-	status = lp.intopt(iocp)
-
-	if status != 'optimal' :
-		raise RuntimeError( 'Error while solving...: ' + status )
-
-	# fix the binary variable to its compute
-	# value and find exact solution
-
-	for d in range( D ) :
+		# a resource is used only once
 
 		for i in range( N ) :
 
-			val = lp.mip_col_val( x( d , i ) )
-			lp.set_col_bnds( x( d , i ) , val , val )
+			mapping = { x( d , i ) : 1 for d in range( D ) }
 
-	smcp = glpk.SimplexControls( ) # (default) simplex control parameters
-	smcp.presolve = True
+			lp.set_mat_row( D + i + 1 , mapping )
 
-	status = lp.simplex(smcp)  # solve
+			lp.set_row_bnds( D + i + 1 , None , 1 )
 
-	if status != 'optimal' :
-		raise RuntimeError( 'Error while solving...: ' + status )
+		lp.set_obj_dir( 'maximize' )
 
-	smcp.presolve = False
-	status = lp.exact(smcp)  # now solve exactly
-
-	if status != 'optimal' :
-		raise RuntimeError( 'Error while solving...: ' + status )
-
-	print( lp.get_obj_val( ) )
+		return lp
 
 
-def solution ( D , N , lp ) :
+	def solve ( D , N , lp ) :
 
-	x = lambda d , i  : d * N + i + 1
+		x = lambda d , i  : d * N + i + 1
 
-	return ( lp.mip_col_val( x( d , i ) ) for d in range( D ) for i in range( N ) )
+		# configure and solve
+
+		iocp = glpk.IntOptControls() # (default) int. opt. control parameters
+		iocp.presolve = True
+
+		status = lp.intopt(iocp)
+
+		if status != 'optimal' :
+			raise RuntimeError( 'Error while solving...: ' + status )
+
+		# fix the binary variable to its compute
+		# value and find exact solution
+
+		for d in range( D ) :
+
+			for i in range( N ) :
+
+				val = lp.mip_col_val( x( d , i ) )
+				lp.set_col_bnds( x( d , i ) , val , val )
+
+		smcp = glpk.SimplexControls( ) # (default) simplex control parameters
+		smcp.presolve = True
+
+		status = lp.simplex(smcp)  # solve
+
+		if status != 'optimal' :
+			raise RuntimeError( 'Error while solving...: ' + status )
+
+		smcp.presolve = False
+		status = lp.exact(smcp)  # now solve exactly
+
+		if status != 'optimal' :
+			raise RuntimeError( 'Error while solving...: ' + status )
+
+		print( lp.get_obj_val( ) )
+
+
+	def solution ( D , N , lp ) :
+
+		x = lambda d , i  : d * N + i + 1
+
+		return ( lp.mip_col_val( x( d , i ) ) for d in range( D ) for i in range( N ) )
+
+except ImportError as e :
+
+	_e = e
+
+	def notify ( *args , **kwargs ) : raise( e )
+
+	knapsack = solver = solution = notify
+
